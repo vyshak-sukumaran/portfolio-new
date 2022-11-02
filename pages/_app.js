e2aa29739eb26f57ed9f8b0bf795743d82577c9b
vyshak-sukumaran <vyshakvyshu791@gmail.com>
2022-11-03 00:28:21 +0530

    error handle

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import React from 'react'
+import ErrorBoundary from '../utils/ErrorBoundary'
 
 function MyApp({ Component, pageProps }) {
 
-  return <Component {...pageProps} />
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
 }
 
 export default MyApp
